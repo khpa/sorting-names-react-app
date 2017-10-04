d8efd3d8daef06a12bcb40cb815c76bb4e643952
Christian Paun <christianpaun@gmail.com>
2017-10-04 19:36:53 +0100

    second deploy, adding remove and highlight

--- src/App.js
+++ src/App.js
@@ -2,66 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class MyComponent extends React.Component {
-  render() {
-    return <div class="">
-      <h1>{this.props.text}</h1>
-      <p>{this.props.par}</p>
-    </div>
-  }
-}
-
-class MySecondComponent extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      txt: 1
-    }
-  }
-
-  random(number) {
-    this.setState({ txt: number })
-  }
-  render() {
-    return <div>
-      <h1>{this.props.text}</h1>
-      <p>{this.props.par}</p>
-      <p>{this.state.txt}</p>
-      <button onClick={() => this.random(Math.random())}>Increment</button>
-    </div>
-  }
-}
-class MyThirdComponent extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      ThirdCompP: "that returns a component",
-      buttonLabel: "click me"
-    }
-  }
-  render() {
-    return (
-      <div>
-        <h1>{this.props.label}</h1>
-        <p>{this.state.ThirdCompP}</p>
-        <button onClick={() => this.setState({ buttonLabel: "I've tricked you!", ThirdCompP: "HaHaHa" })}>{this.state.buttonLabel}</button>
-      </div>)
-  }
-}
-
 class ContComponent extends React.Component {
   constructor() {
     super();
     this.state = {
       names: [],
-      label: 0
+      label: 0,
+      highlight: "",
+      teststate: ""
     }
   }
 
   listaNume(lista) {
+    var highlight = this.state.highlight;
     var result = lista.sort();
+  
+
+    let removeFromAry = (i) => {
+      console.log(i + 1);
+      var filteredAry = lista.filter(e => e !== this.state.names[i])
+
+      this.setState({ names: filteredAry, label:this.state.label-1})
+    };
+  
     result = lista.map(function (item) {
-      return <p>{item}</p>;
+      if (item === highlight) {
+        return (
+          <div>
+            <p className="blue">
+              <b>{item}</b>
+              <button onClick={() => removeFromAry(lista.indexOf(item)) + 1}>remove</button>
+            </p>
+          </div>);
+      }
+      else
+        return <div><p>{item}<button onClick={() => removeFromAry(lista.indexOf(item)) + 1}>remove</button></p></div>;
     });
 
     return result;
@@ -71,26 +46,24 @@ class ContComponent extends React.Component {
     const lista = this.listaNume(this.state.names);
     return (
       <div>
-        <div class="List-container">
+        <h1>Just a list of names... sorted... and more :)</h1>
+        <div className="List-container">
           {lista}
         </div>
-        {/*   <MyComponent text="Hello World" par="This is test subtitle" />
-        <MySecondComponent text="I am a component" par="I am the above comp subtitle" />
-        <MyThirdComponent label={this.state.label} /> 
-   */}
+        
         <p>Total names: {this.state.label}</p>
-        <input type="text" value={this.state.value} onChange={event => { console.log(event); this.setState({ value: event.target.value }) }} />
+        <input type="text" value={this.state.value} onChange={event => { this.setState({ value: event.target.value }) }} />
         <p>Last Name sent: {this.state.value}</p>
-        <button onClick={() => this.setState({ names: [], label:0 })}>Clear list</button>
-        <button onClick={() => this.setState((state) => state.names.push(this.state.value), this.setState({ label: this.state.names.length + 1 }))}>Add to list</button>
-
+        <button onClick={() => this.setState({ names: [], label: 0 })}>Clear list</button>
+        <button onClick={() => this.setState((state) => state.names.push(this.state.value), this.setState({ label: this.state.names.length + 1, highlight: this.state.value }))}>Add to list</button>
+<p className="copyright">just a copyright note</p>
       </div>)
   }
 }
 class App extends Component {
   render() {
     return (
-      <div class="App-main">
+      <div className="App-main">
         <ContComponent />
       </div>
     );
