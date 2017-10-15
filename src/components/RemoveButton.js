a228857380ee13f0432e2a345f09a02b2d1c9aae
Gabriel Rusu <gabriel.rusu@finleap.com>
2017-10-15 21:39:29 +0100

    Updated

--- src/components/RemoveButton.js
+++ src/components/RemoveButton.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-class RemoveButton extends React.Component {
-
-    
+const RemoveButton = function({item, actionFct, className}) {
+    return (
+        <div>
+          <p className={ className }>
+            <b>{ item }</b>
+            <button onClick={ () => actionFct(item) }>remove</button>
+          </p>
+        </div>)
 }
+
 export default RemoveButton;
