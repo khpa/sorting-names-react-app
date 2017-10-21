74183b2e94f61f7527afa8952834ba5eb95b736f
Christian Paun <christianpaun@gmail.com>
2017-10-21 02:58:55 +0100

    sort & h. color fixed, remove fixed, read from API added - 2

--- src/components/NameLabel.js
+++ src/components/NameLabel.js
@@ -15,16 +15,14 @@ class NameLabel extends React.Component {
       teststate: ""
     }
   }
- 
- 
 
   listaNume(lista) {
 
-    lista.sort(function compare(a, b) { return a.name < b.name ? -1 : a.name > b.name ? 1 : 0; }); //sort profi ^_^, me proud :D
-   
+    lista.sort(function compare(a, b) { return a.name < b.name ? -1 : a.name > b.name ? 1 : 0; }); //sort 
+
     let removeFromAry = (i) => {
       let filteredAry = lista;
-      let index = filteredAry.findIndex(x => x.name === i);    
+      let index = filteredAry.findIndex(x => x.name === i);
       console.log('index of ' + i + ' is:' + index);
       filteredAry.splice(index, 1);
       this.setState({
@@ -32,34 +30,12 @@ class NameLabel extends React.Component {
         label: this.state.names.length
       })
     }
-    // let removeFromAry = (i) => {
-    //   console.log('i este: ' + i);
-    //   var position = lista.indexOf(i);
-    //   console.log('lista' + lista);
-    //   console.log('position in ary:' + position);
-    //   //var filteredAry = lista.filter(e => e !== this.state.names[i])
-    //   let filteredAry = lista;
-    //   console.log('filtered: ' + filteredAry)
-    //   //const filteredAry = new Set(lista);
-    //   //filteredAry.delete(i);
 
-    //   filteredAry.splice(position, 1);
-    //   console.log('dupa splice ' + filteredAry);
-
-    //   //console.log (typeof filteredAry); 
-
-    //   this.setState({
-    //     names: filteredAry,
-    //     label: this.state.names.length
-    //   })
-    // };
     console.log(lista);
-    
 
     lista.sort(function (a, b) { return (a.nume > b.nume) ? 1 : ((b.nume > a.nume) ? -1 : 0); });
 
     return (
-
       lista.map((item) => {
         return (
           <RemoveButton item={item.name} actionFct={removeFromAry} className={(item.name === this.state.highlight && item.isLast === true) ? "blue" : ""} />
@@ -68,14 +44,13 @@ class NameLabel extends React.Component {
   }
 
   componentWillMount() {
-    
-        api.getRovers().then((res) => {
-          this.setState({
-            names: res,
-            label: res.length
-          })
-        });
-      }
+    api.getUsers().then((res) => {
+      this.setState({
+        names: res,
+        label: res.length
+      })
+    });
+  }
 
   foo = () => {
     var newArray = this.state.names.map(function reset(value, index) {
@@ -85,27 +60,17 @@ class NameLabel extends React.Component {
       };
     }
     )
-this.state.names = newArray;
+
+    this.state.names = newArray;
     this.state.names.push({ name: this.state.value, isLast: true }),
-    this.setState({
-      label: this.state.names.length,
-      highlight: this.state.value
-    
-    })
-
-  console.log('uraaaaa')
-
+      this.setState({
+        label: this.state.names.length,
+        highlight: this.state.value
+      })
   };
-
-  // foo = ()=> {
-  //   console.log('foo1');
-  //   console.log('foo2');
-  // }
 
   render() {
     const lista = this.listaNume(this.state.names);
-    console.log("Rovers", this.state.rovers);
-    //objs.sort(function(a,b) {return (a.nume > b.nume) ? 1 : ((b.nume > a.nume) ? -1 : 0);} );
     return (
       <div>
         <h1>Just a list of names... sorted... and more :)</h1>
@@ -115,31 +80,24 @@ this.state.names = newArray;
         <p>Total names:
           {this.state.label}
         </p>
+        
+        <p>Last Name sent:
+          {this.state.value}
+        </p>
         <input type="text" value={this.state.value} onChange={event => {
           this.setState({
             value: event.target.value
           })
         }} />
-        <p>Last Name sent:
-          {this.state.value}
-        </p>
+        <div className="buttons-container">
         <button onClick={() => this.componentWillMount()}>LoadFromApi</button>
-       
+
         <button onClick={this.foo}>AddNamesV2</button>
         <button onClick={() => this.setState({
           names: [],
           label: 0,
-          rovers:[]
+          rovers: []
         })}>ClearList</button>
-
-        {/* this.state.names.map(function reset (value,index) {
-  return {
-    id:index,
-  name:value.name,
-    isLast:false
-  }; */}
-
-
 
         <button onClick={() => this.setState((state) =>
           state.names.push({ name: this.state.value, isLast: true }),
@@ -149,6 +107,7 @@ this.state.names = newArray;
           })
         )
         }>Add to list</button>
+        </div> 
         <p className="copyright">JSON: <a href="https://api.myjson.com/bins/18gptr" target="_blank">https://api.myjson.com/bins/18gptr</a></p>
       </div>)
   }
